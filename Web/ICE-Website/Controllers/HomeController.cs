6d1160e32d7dcebe934f9fbf94d596611b6e687f
mr-andersonn <anton-anderson_n@proton.me>
2026-03-22 13:30:53 +0100

    Website init

---
+++ Web/ICE-Website/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ICE_Website.Models;
+
+namespace ICE_Website.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Web/ICE-Website/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ICE_Website.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
