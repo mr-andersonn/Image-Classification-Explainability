f38a041978cc359798deee4c1d55b404c8416cac
mr-andersonn <anton-anderson_n@proton.me>
2026-04-29 12:59:31 +0200

    Slight website changes

--- Web/ICE-Website/obj/Debug/net10.0/ICE-Website.AssemblyInfo.cs
+++ Web/ICE-Website/obj/Debug/net10.0/ICE-Website.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ICE-Website")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f98ab67ceea8da3cabfa9871f91a40fd16457a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f668c037f99a3c4c5c05babdb84276e3f931501")]
 [assembly: System.Reflection.AssemblyProductAttribute("ICE-Website")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ICE-Website")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
